205f96aea14d3950d1a83995dbf639ab90f14abc
Arseniy Ivanov <138289+freeatnet@users.noreply.github.com>
2023-06-18 07:55:33 -0600

    feat: use edgedb (#3)
    * add sample usage of edgedb
    
    * set up autogenerated files in CI
    
    * unify `EDGEDB_INSTANCE_NAME` in Next.js CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const serverTime = api.example.currentTime.useQuery();
 
   return (
     <>
@@ -44,6 +45,9 @@ export default function Home() {
           </div>
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+          </p>
+          <p className="text-2xl text-white">
+            {serverTime.data ? serverTime.data : "Getting database time..."}
           </p>
         </div>
       </main>
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { e } from "~/server/edgedb";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -10,4 +11,10 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
+  currentTime: publicProcedure.query(async ({ ctx: { edgedbClient } }) => {
+    const query = e.select(e.cast(e.str, e.datetime_current()));
+    const time = await query.run(edgedbClient);
+
+    return time;
+  }),
 });
